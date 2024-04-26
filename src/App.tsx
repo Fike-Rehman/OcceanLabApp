08423e67f90150202800d63f3d4b5069e3f2f853
frehman <fike.rehman@hotmail.com>
2024-04-25 19:15:24 -0500

    initial check in

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Welcome from './components/Welcome'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Welcome></Welcome>
+      <div>App crearted with the help of Digital Ocean</div>
+      <a href="https://www.digitalocean.com/community/tutorials/how-to-set-up-a-react-project-with-vite">Go here</a>
+    </>
+  )
+}
+
+export default App
