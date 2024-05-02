4066b604efed6fc5dfc06a7c55c862674c3330db
frehmanOnQC <fike.rehman@gmail.com>
2024-05-02 16:58:50 -0500

    updated welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Welcome from './components/Welcome'
+import Welcome from './components/Welcome/Welcome'
 
 function App() {
   // const [count, setCount] = useState(0)
