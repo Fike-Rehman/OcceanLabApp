1be1d35f632e34de5b87c9e871b7a735a1690019
frehman <fike.rehman@hotmail.com>
2024-09-08 18:10:42 -0500

    dynamic background on load

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
+
 export default function Welcome() {
-    return (
+  // const [backgroundImage, setBackgroundImage] = useState("");
+
+  useEffect(() => {
+    const images = [
+      "/images/bkgImage1.jpg",
+      "/images/bkgImage2.jpg",
+      "/images/bkgImage3.jpg",
+      "/images/bkgImage4.jpg",
+    ];
+
+    // Select a random image
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    document.body.style.backgroundImage = `url(${randomImage})`;
+
+    //  setBackgroundImage(randomImage);
+  }, []);
+
+  // const backgroundStyle = {
+  //   backgroundImage: `url(${backgroundImage})`,
+  //   backgroundSize: "cover",
+  //   backgroundPosition: "center",
+  //   backgroundRepeat: "no-repeat",
+  //   height: "100vh",
+  //   width: "100%",
+  //   flex: 1,
+  // };
+
+  return (
     <>
-    <div className="wrapper">
-    <h1>Welcome To My App</h1>
-    <p>This is going to be the coolest app in the world!</p>
-    </div>
+      <div>
+        <h1>Welcome To My App</h1>
+        <p>This is going to be the coolest app in the world!</p>
+      </div>
     </>
-    );
-    }
+  );
+}
